450444e2013eda0459b3ae3f4b869a3b4e4e3b4b
Taheir Ahmed Ashraf <programmerashraf001@gmail.com>
2021-07-09 22:01:14 +0600

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export default function about() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>lorem loremloremloremloremloremloremloremloremloremlorem</p>
+        </div>
+    )
+}
